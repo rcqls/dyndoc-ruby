6c81e4b98eb554c2831b8d928e01d23470b2bd53
R cqls <rdrouilh@gmail.com>
2016-02-22 21:54:54 +0100

    dyndoc dir template

---
+++ bin/dyn-init.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+
+share_path=File.expand_path(“../../share”, __FILE__)
+dyndoc_path=File.join(ENV["HOME"],"dyndoc")
+unless File.exist? dyndoc_path
+	FileUtils.mkdir_p dyndoc_path
+	FileUtils.cp_r File.join(share_path,"."),dyndoc_path
+end
