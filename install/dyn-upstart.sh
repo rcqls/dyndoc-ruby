93f752b69a57bfc847e361d4c775839eebd879b8
R cqls <rdrouilh@gmail.com>
2016-03-26 17:13:19 +0100

    add upstart

---
+++ install/dyn-upstart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for cmd in "dyn-srv" "dyntask-server"
+do
+  conf = "/etc/init/${cmd}.conf"
+  sudo echo "author \"rcqls\"" > ${conf}
+  sudo echo "description \"start and stop dyn-srv for Ubuntu (upstart)\"" >> ${conf}
+  sudo echo "version \"0.1\"" >> ${conf}
+  sudo echo "" >> ${conf}
+  sudo echo "start on started networking" >> ${conf}
+  sudo echo "stop on runlevel [!2345]" >> ${conf}
+  sudo echo "" >> ${conf}
+  sudo echo "env APPUSER=\"${USER}\"" >> ${conf}
+  sudo echo "env APPDIR=\"/home/${USER}/.gem/ruby/2.2.0/bin\"" >> ${conf}
+  sudo echo "env APPBIN=\"${cmd}\"" >> ${conf}
+  sudo echo "" >> ${conf}
+  sudo echo "respawn" >> ${conf}
+  sudo echo "" >> ${conf}
+  sudo echo "script" >> ${conf}
+  sudo echo "  exec su - $APPUSER -c \"$APPDIR/$APPBIN\"" >> ${conf}
+  sudo echo "end script" >> ${conf}
+done
--- install/dyndoc_install_ubuntu.sh
+++ install/dyndoc_install_ubuntu.sh
@@ -50,30 +50,60 @@ fi
 echo "check if ~/.gemrc exists ..."
 if ![ -e "$HOME/.gemrc" ]; then
 	echo "~/.gemrc config ..."
-	echo "gem: --user-install --no-ri --no-rdoc"
+	echo "gem: --user-install --no-ri --no-rdoc" >> ~/.gemrc
+	echo "" >> ~/.bash_profile
+	echo "if which ruby >/dev/null && which gem >/dev/null; then" >> ~/.bash_profile
+  echo "PATH=\"$(ruby -rubygems -e 'puts Gem.user_dir')/bin:$PATH\"" >> ~/.bash_profile
+	echo "fi" >> ~/.bash_profile
 fi
 
 echo "Installing dyndoc (ruby part) and its dependencies ..."
 gem install dyndoc-ruby
+gem install dyntask-ruby
 
 echo "Installing dyndoc (R part) and its dependencies ..."
 # devtools dependencies
 sudo apt-get install -y libxml2-dev  libcurl4-openssl-dev libssl-dev
 sudo R -e 'install.packages("devtools",repos="http://cran.rstudio.com/")'
-
 # R package rb4R
 sudo R -e 'devtools::install_github("rcqls/rb4R",args="--no-multiarch")'
-# if something goes wrong in the previous instruction redo after: sudo apt-get install libgmp-dev
-
 # R package base64
 sudo R -e 'install.packages("base64",repos="http://cran.rstudio.com/")'
+
+echo "dyndoc init"
+. ~/.bash_profile
+dyn-init
+dyntask-init
+
+read -p "Do you want to install dyn-srv.conf and dyntask.conf upstart [Y/N]" -n 1 -r
+echo
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+	for cmd in "dyn-srv" "dyntask-server"
+	do
+		conf = "/etc/init/${cmd}.conf"
+		sudo echo "author \"rcqls\"" > ${conf}
+		sudo echo "description \"start and stop dyn-srv for Ubuntu (upstart)\"" >> ${conf}
+		sudo echo "version \"0.1\"" >> ${conf}
+		sudo echo "" >> ${conf}
+		sudo echo "start on started networking" >> ${conf}
+		sudo echo "stop on runlevel [!2345]" >> ${conf}
+		sudo echo "" >> ${conf}
+		sudo echo "env APPUSER=\"${USER}\"" >> ${conf}
+		sudo echo "env APPDIR=\"/home/${USER}/.gem/ruby/2.2.0/bin\"" >> ${conf}
+		sudo echo "env APPBIN=\"${cmd}\"" >> ${conf}
+		sudo echo "" >> ${conf}
+		sudo echo "respawn" >> ${conf}
+		sudo echo "" >> ${conf}
+		sudo echo "script" >> ${conf}
+		sudo echo "  exec su - $APPUSER -c \"$APPDIR/$APPBIN\"" >> ${conf}
+		sudo echo "end script" >> ${conf}
+	done
+fi
 
 echo "check pdflatex in PATH"
 if [ "$(which pdflatex)" = "" ]; then
 	echo "Installing pdflatex! Be patient, this is a long step but the last ..."
  	sudo apt-get install -y texlive-full
 fi
-
-# R package devtools
 
 cd ${prevdir}
