069a6211aa99e6e7a0da242be9c8d5ceb66e1069
R cqls <rdrouilh@gmail.com>
2016-03-27 08:35:39 +0200

    upstart add

--- install/dyndoc_install_ubuntu.sh
+++ install/dyndoc_install_ubuntu.sh
@@ -80,23 +80,24 @@ echo
 if [[ "$REPLY" =~ ^[Yy]$ ]]; then
 	for cmd in "dyn-srv" "dyntask-server"
 	do
-		conf = "/etc/init/${cmd}.conf"
-		sudo echo "author \"rcqls\"" > ${conf}
-		sudo echo "description \"start and stop dyn-srv for Ubuntu (upstart)\"" >> ${conf}
-		sudo echo "version \"0.1\"" >> ${conf}
-		sudo echo "" >> ${conf}
-		sudo echo "start on started networking" >> ${conf}
-		sudo echo "stop on runlevel [!2345]" >> ${conf}
-		sudo echo "" >> ${conf}
-		sudo echo "env APPUSER=\"${USER}\"" >> ${conf}
-		sudo echo "env APPDIR=\"/home/${USER}/.gem/ruby/2.2.0/bin\"" >> ${conf}
-		sudo echo "env APPBIN=\"${cmd}\"" >> ${conf}
-		sudo echo "" >> ${conf}
-		sudo echo "respawn" >> ${conf}
-		sudo echo "" >> ${conf}
-		sudo echo "script" >> ${conf}
-		sudo echo "  exec su - $APPUSER -c \"$APPDIR/$APPBIN\"" >> ${conf}
-		sudo echo "end script" >> ${conf}
+	  conf="${cmd}.conf"
+	  echo "author \"rcqls\"" > "${conf}"
+	  echo "description \"start and stop dyn-srv for Ubuntu (upstart)\"" >> "${conf}"
+	  echo "version \"0.1\"" >> "${conf}"
+	  echo "" >> "${conf}"
+	  echo "start on started networking" >> "${conf}"
+	  echo "stop on runlevel [!2345]" >> "${conf}"
+	  echo "" >> "${conf}"
+	  echo "env APPUSER=\"${USER}\"" >> "${conf}"
+	  echo "env APPDIR=\"/home/${USER}/.gem/ruby/2.2.0/bin\"" >> "${conf}"
+	  echo "env APPBIN=\"${cmd}\"" >> "${conf}"
+	  echo "" >> "${conf}"
+	  echo "respawn" >> "${conf}"
+	  echo "" >> "${conf}"
+	  echo "script" >> "${conf}"
+	  echo '  exec su - $APPUSER -c "$APPDIR/$APPBIN"' >> "${conf}"
+	  echo "end script" >> "${conf}"
+	  sudo mv "${conf}" /etc/init/
 	done
 fi
 
