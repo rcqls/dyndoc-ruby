22f0807939338a30e92b61d86c746919588fa9f1
R Cqls <rdrouilh@gmail.fr>
2016-09-28 13:15:26 +0200

    add multidoc

--- dyndoc-ruby.gemspec
+++ dyndoc-ruby.gemspec
@@ -2,7 +2,7 @@
 require 'rubygems/package_task'
 
 pkg_name='dyndoc-ruby'
-pkg_version='0.7.7'
+pkg_version='0.8.0'
 
 pkg_files=FileList[
     'bin/*',
--- lib/dyndoc-convert.rb
+++ lib/dyndoc-convert.rb
@@ -40,25 +40,25 @@ module Dyndoc
   end
 
   ## TODO: a config.yml file for the site
-  def Dyndoc.cli_convert_from_file(dyn_file,html_file,root={}) #ex: root={dyn: , html: }
+  def Dyndoc.cli_convert_from_file(dyn_file,html_file,opts={}) #ex: opts={dyn_root: , html_root:, user: , doc_tag: }
     addr="127.0.0.1"
 
-    return unless root[:dyn]
+    return unless opts[:dyn_root]
 
     dyn_libs,dyn_tags=nil,nil
 
-    ## requirement: dyn_file is provided relatively to the root[:dyn] (for security reason too)
+    ## requirement: dyn_file is provided relatively to the opts[:dyn_root] (for security reason too)
 
     dyn_path=dyn_file.split(File::Separator)
 
-    dyn_file=File.join(root[:dyn],dyn_file) unless dyn_file[0]=="/"
+    dyn_file=File.join(opts[:dyn_root],dyn_file) unless dyn_file[0]=="/"
 
     if i=(dyn_file =~ /\_?(?:html)?\.dyn$/)
 
       cfg={}
       ## find the previous config.yml in the tree folder
       ## TODO: read all previous config.yml and merge them from root to current
-      cfg_yml_files=dyn_path.inject([""]) {|res,e| res + [(res[-1,1]+[e]).flatten]}.map{|pa| File.join(root[:dyn],pa,"config.yml")}.reverse
+      cfg_yml_files=dyn_path.inject([""]) {|res,e| res + [(res[-1,1]+[e]).flatten]}.map{|pa| File.join(opts[:dyn_root],pa,"config.yml")}.reverse
       cfg_yml_file=cfg_yml_files.select{|c| File.exists? c}[0]
       cfg=YAML::load_file(cfg_yml_file) if cfg_yml_file
 
@@ -83,22 +83,22 @@ module Dyndoc
       page=nil
 
       if code =~ /^\-{3}/
-        b=code.split(/^\-{3}/)
-        if b[0].empty?
+        b=code.split(/^(\-{3,})/,-1)
+        if b[0].empty? and b.length>4
           require 'yaml'
-          page=YAML.load(b[1])
+          page=YAML.load(b[2])
           cfg.merge!(page)
-          code=b[2..-1].join("---")
+          code=b[4..-1].join("")
         end
       end
 
       # dyn_root can be overwritten by cfg
-      dyn_root= cfg["dyn_root"] || root[:dyn] || File.expand_path("..",dyn_file)
-      html_root= cfg["html_root"] || root[:html] || File.expand_path("..",dyn_file)
+      dyn_root= cfg["dyn_root"] || opts[:dyn_root] || File.expand_path("..",dyn_file)
+      html_root= cfg["html_root"] || opts[:html_root] || File.expand_path("..",dyn_file)
 
       if cfg["layout"]
         if cfg["layout"][0] == "%" #user mode
-          cfg_tmp=File.join(root[:dyn],'users',root[:user],cfg["layout"][1..-1])
+          cfg_tmp=File.join(opts[:dyn_root],'users',opts[:user],cfg["layout"][1..-1])
         else
           cfg_tmp=File.join(dyn_root,cfg["layout"][0] == "/" ? cfg["layout"][1..-1] : ["layout",cfg["layout"]])
         end
@@ -106,7 +106,7 @@ module Dyndoc
       end
       if cfg["pre"]
         if cfg["pre"][0] == "%" #user mode
-          cfg_tmp=File.join(root[:dyn],'users',root[:user],cfg["pre"][1..-1])
+          cfg_tmp=File.join(opts[:dyn_root],'users',opts[:user],cfg["pre"][1..-1])
         else
           #cfg_tmp=File.join(dyn_root,cfg["pre"])
           cfg_tmp=File.join(dyn_root,cfg["pre"][0] == "/" ? cfg["pre"][1..-1] : ["preload",cfg["pre"]])
@@ -116,15 +116,13 @@ module Dyndoc
 
       if cfg["post"]
         if cfg["post"][0] == "%" #user mode
-          cfg_tmp=File.join(root[:dyn],'users',root[:user],cfg["post"][1..-1])
+          cfg_tmp=File.join(opts[:dyn_root],'users',opts[:user],cfg["post"][1..-1])
         else
           #cfg_tmp=File.join(dyn_root,cfg["post"])
           cfg_tmp=File.join(dyn_root,cfg["post"][0] == "/" ? cfg["post"][1..-1] : ["postload",cfg["post"]])
         end
         dyn_post_code=File.read(cfg_tmp) unless dyn_post_code and File.exist? cfg_tmp
       end
-
-
 
       ## deal with html_file
       html_file=File.join(html_root,cfg["html_file"] || html_file)
@@ -136,7 +134,14 @@ module Dyndoc
 
       dyn_libs=cfg["libs"].strip if cfg["libs"]
 
-      dyn_tags="[#<]{#opt]"+cfg["tags"].strip+"[#opt}[#>]" if cfg["tags"]
+      ## mode multi-documents
+      docs_tags=[]
+      docs_tags << opts[:doc_tag] if opts[:doc_tag]
+      ## complete docs_tags with cfg["tags"]
+      docs_tags += (cfg["tags"]||"").split(",").map{|e| e.strip}
+      dyn_tags="[#<]{#opt]"+docs_tags.join(",")+"[#opt}[#>]" unless docs_tags.empty?
+
+      ### Dyndoc.warn  :dyn_tags,[docs_tags,dyn_tags]
 
     	if dyn_libs or dyn_pre_code
     		code_pre = ""
@@ -149,7 +154,17 @@ module Dyndoc
       code = "[#rb<]require 'ostruct';cfg = OpenStruct.new(" + cfg.inspect + ")[#>]" +code
       code = "[#rb<]page = " + page.inspect + "[#>]" +code if page
       code = dyn_tags + code if dyn_tags
-    	dyndoc_start=[:dyndoc_libs,:dyndoc_layout]
+
+      ## add path for user
+      code_path = "[#path]"+File.join(opts[:dyn_root],'users',opts[:user],"dynlib")
+      code_path << "\n" << File.join(opts[:dyn_root],'users',opts[:user])
+      code_path << "\n" << opts[:dyn_root] << "\n"
+      code_path << "[#main][#<]\n"
+      code = code_path + code
+
+      ### Dyndoc.warn :code,code
+
+      dyndoc_start=[:dyndoc_libs,:dyndoc_layout]
 
       cli=Dyndoc::InteractiveClient.new(code,dyn_file,addr,dyndoc_start)
 
