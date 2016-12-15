08188ed710987be879712fbbccece1aa8d4d3645
R Cqls <rdrouilh@gmail.fr>
2016-12-15 22:21:57 +0100

    first attempt for dun-auto

--- lib/dyndoc-convert.rb
+++ lib/dyndoc-convert.rb
@@ -238,4 +238,196 @@ module Dyndoc
     end
   end
 
+
+  ## Very similar to Dyndoc.process_html_file_from_dyn_file
+  def Dyndoc.auto_dyn_file(code,output_file,dyn_file,dyn_layout,addr,dyndoc_start)
+    cli=Dyndoc::InteractiveClient.new(code,dyn_file,addr,dyndoc_start)
+
+    if dyn_layout
+      cli=Dyndoc::InteractiveClient.new(File.read(dyn_layout),"",addr) #File.expand_path(dyn_layout),addr)
+    end
+
+    if output_file and Dir.exist? File.dirname(output_file)
+      File.open(output_file,"w") do |f|
+        f << cli.content
+      end
+    else
+      puts cli.content
+    end
+  end
+
+  ###################################
+  ## Dyndoc.auto_convert_from_file ##
+  ###########################################################################
+  ## Very similar to Dyndoc.cli_convert_from_file which is (badly) devoted to html file!
+  ## This version tries to autoconvert a dyn_file of the form *_<format>.dyn to *.<format>
+  ## Multi-documents is not considered here! The goal is to provide autoconversion for simple file (even though we can use template)
+  ## :format_output => "html" is not a pb since it is mostly used for verbatim output and it is not the focus here!
+
+  def Dyndoc.auto_convert_from_file(dyn_file,opts={}) #ex: opts={dyn_root: , doc_tag: } #opts=obtained by commandline
+    ## opts[:dyn_root] ||= ENV["HOME"]
+
+    dyn_libs,dyn_tags,dyn_layout,dyn_pre_code,dyn_post_code=nil,nil,nil,nil,nil
+
+    dyn_path=dyn_file.split(File::Separator)
+
+
+    if i=(dyn_file =~ /\_([a-z,A-Z,0-9]*)\.dyn$/)
+      dyn_extname = $1
+      dyn_dirname = File.dirname(dyn_file[0...i])
+      dyn_basename = dyn_file[0...i]
+
+      cfg_files={
+        fullname: dyn_file,
+        basename: File.basename(dyn_file,".*"),
+        tag:  opts[:doc_tag],
+        dyn_extname: dyn_extname,
+        dyn_dirname: dyn_dirname,
+        dyn_basename: dyn_basename,
+        output_basename: dyn_basename+"."+dyn_extname
+      }
+
+      cfg={}
+
+      ## find the previous config.yml in the tree folder
+      ## TODO: read all previous config.yml and merge them from root to current
+      ## The merge could be also to join the content when the key is the same.
+      ## Ex: semantic-ui (1st config.yml): css_message
+      ##    semantic-ui (2nd config.yml): css_icon
+      ##  becomes: semantic-ui: css_message, css_icon
+      ## NEEDS TO DECLARE the fields that behave like this in some other config file (keys.yml)
+
+      cfg_yml_files=dyn_path.inject([""]) {|res,e| res + [(res[-1,1]+[e]).flatten]}.map{|pa| File.join("",pa,"config.yml")}.reverse
+      cfg_yml_file=cfg_yml_files.select{|c| File.exists? c}[0]
+      cfg=YAML::load_file(cfg_yml_file) if cfg_yml_file
+
+      ## add info related to dyn file
+      cfg.merge!(cfg_files)
+
+      ## Dyn layout
+      dyn_layout=dyn_file[0...i]+"_#{dyn_extname}-layout.dyn" if File.exist? dyn_file[0...i]+"_#{dyn_extname}-layout.dyn"
+
+      ## Dyn pre
+      dyn_pre_code=File.read(dyn_file[0...i]+"_#{dyn_extname}-pre.dyn") if File.exist? dyn_file[0...i]+"_#{dyn_extname}-pre.dyn"
+
+      ## Dyn post
+      dyn_post_code=File.read(dyn_file[0...i]+"_#{dyn_extname}-post.dyn") if File.exist? dyn_file[0...i]+"_#{dyn_extname}-post.dyn"
+
+
+      if File.exist? dyn_file[0...i]+"_#{dyn_extname}.dyn_cfg" and (yml=YAML::load_file(dyn_file[0...i]+"_#{dyn_extname}.dyn_cfg"))
+        cfg.merge!(yml)
+      else # try do find (in the Zope spirit) a config file in the nearest folder
+      end
+
+      ## code to evaluate
+      code=File.read(dyn_file)
+
+      page={}
+
+      if code =~ /^\-{3}/
+        b=code.split(/^(\-{3,})/,-1)
+        if b[0].empty? and b.length>4
+          require 'yaml'
+          page=YAML.load(b[2])
+          cfg.merge!(page)
+          code=b[4..-1].join("")
+        end
+      end
+
+      # dyn_root can be overwritten by cfg
+      # defines the root of relative predefined dyn (pre, post, ...) files
+      dyn_root= cfg["dyn_root"] || opts[:dyn_root] || File.expand_path("..",dyn_file)
+
+      cfg["layout"] = cfg["pre"] = cfg["post"] = cfg["model"] if cfg["model"]
+
+      if cfg["layout"]
+        if cfg["layout"][0] == "/"
+          cfg_tmp=cfg["layout"]
+        else
+          cfg_tmp=File.join(dyn_root, cfg["layout"])
+        end
+        cfg_tmp+= ".dyn" if File.extname(cfg_tmp).empty?
+        ##Dyndoc.warn :layout,cfg_tmp
+        dyn_layout=cfg_tmp if !dyn_layout and File.exist? cfg_tmp
+      end
+      if cfg["pre"]
+        if cfg["pre"][0] == "/"
+          cfg_tmp=cfg["pre"]
+        else
+          cfg_tmp=File.join(dyn_root,cfg["pre"])
+        end
+        cfg_tmp+= ".dyn" if File.extname(cfg_tmp).empty?
+        dyn_pre_code=File.read(cfg_tmp) if !dyn_pre_code and File.exist? cfg_tmp
+      end
+
+      if cfg["post"]
+        if cfg["post"][0] == "/"
+          cfg_tmp=cfg["post"]
+        else
+          cfg_tmp=File.join(dyn_root,cfg["post"])
+        end
+        cfg_tmp+= ".dyn" if File.extname(cfg_tmp).empty?
+        dyn_post_code=File.read(cfg_tmp) if !dyn_post_code and File.exist? cfg_tmp
+      end
+
+
+      ## deal with html_file
+      output_file=File.join(opts[:where] || cfg["dyn_dirname"],cfg["output_basename"])
+      unless File.exist? output_file
+        dirname=File.dirname(output_file)
+        require 'fileutils'
+        FileUtils.mkdir_p dirname
+      end
+
+      dyn_libs=cfg["libs"].strip if cfg["libs"]
+
+      ## mode multi-documents
+      docs_tags=[]
+      ## since opts[:doc_tag] can have more than one tag it is replaced with __ALL_DOC_TAG__ that is replaced before processing with the proper tag
+      docs_tags << opts[:doc_tag] if opts[:doc_tag]
+      ## complete docs_tags with cfg["tags"]
+      docs_tags += (cfg["tags"]||"").split(",").map{|e| e.strip}
+      dyn_tags="[#<]{#opt]"+docs_tags.join(",")+"[#opt}[#>]" unless docs_tags.empty?
+
+      ### Dyndoc.warn  :dyn_tags,[docs_tags,dyn_tags]
+
+      if dyn_libs or dyn_pre_code
+        code_pre = ""
+        code_pre += dyn_pre_code + "\n" if dyn_pre_code
+        code_pre += '[#require]'+"\n"+dyn_libs if dyn_libs
+        code = code_pre + '[#main][#>]' + code
+      end
+      code += "\n" + dyn_post_code if dyn_post_code
+      ## TO TEST!!!
+      ##
+      Dyndoc.warn :cfg,cfg
+      ##Dyndoc.warn :page,page
+      code = "[#rb<]require 'ostruct';cfg = OpenStruct.new(" + cfg.inspect + ");page = OpenStruct.new(" + page.inspect + ")[#>]" +code
+      code = dyn_tags + code if dyn_tags
+
+      ## add path for user
+      code_path = "[#path]"
+      code_path << "\n" << File.join(dyn_root,'dynlib')
+      code_path << "\n" << opts[:dynlib_root] << "\n" if opts[:dynlib_root]
+      code_path << "\n" << cfg[:dynlib_root] << "\n" if cfg[:dynlib_root]
+      code_path << "[#main][#<]\n"
+      code = code_path + code
+
+      ### Dyndoc.warn :code,code
+
+      dyndoc_start=[:dyndoc_libs,:dyndoc_layout]
+
+      ##unless cfg[:doc_tags]
+        Dyndoc.auto_dyn_file(code,output_file,dyn_file,dyn_layout,addr,dyndoc_start)
+      # else
+      #   (cfg[:doc_tags]).each do |doc_tag|
+      #     output_file = cfg[:outputs][doc_tag] ? (cfg[:outputs][doc_tag][0] == "/" ? cfg[:outputs][doc_tag] :  File.join() ) : File.join(opts[:where] || cfg["dyn_dirname"],cfg["output_basename"])
+      #     p [:html_multi,doc_tag,html_file] #,code.gsub(/__ALL_DOC_TAG__/,doc_tag)]
+      #     Dyndoc.auto_dyn_file(code,html_file,dyn_file,dyn_layout,addr,dyndoc_start)
+      #   end
+      # end
+    end
+  end
+
+
 end
